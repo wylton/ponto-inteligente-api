09dece482fb32a0db1ba755abc9f1af24940d84b
Wilton Lopes <wyltonlopes@hotmail.com>
2017-10-01 18:20:06 -0300

    Commit Inicial dos arquivos do projeto.

---
+++ src/main/java/com/wilton/pontointeligente/api/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.wilton.pontointeligente.api;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PontointeligenteApplication.class);
+	}
+
+}
